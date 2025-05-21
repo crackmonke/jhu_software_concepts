653440ee84cf9dba5722c26389352e800ab44f97
crackmonke <savy.greeley@icloud.com>
2025-05-21 14:12:36 -0700

    finished readings

--- Module 1/readings.py
+++ Module 1/readings.py
@@ -1,1 +1,19 @@
 print("Hello, World!")
+
+#Try the following
+#Perform a mathematical operation, like adding 5 and 4. You can experiment with other operations as well.
+print(5 + 4)
+#Display some text, like your name. Remember to wrap it in quotation marks.
+print("Savannah")
+#Enter import this or import antigravity to get a sneak peek at some of Python’s Easter Eggs.
+import this
+import antigravity
+
+#defining a variable
+greeting = "Hello, World!"
+#Printing the variable
+print(greeting)
+#Making it uppercase
+greeting = greeting.upper()
+#Printing the variable again
+print(greeting)
