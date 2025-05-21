8c8f75886de5947550ea6e27071e6cb8de111073
crackmonke <savy.greeley@icloud.com>
2025-05-21 15:10:20 -0700

    both files done

---
+++ Module 1/Assignments/due_date.py
@@ -0,0 +1,16 @@
+# 2. For the second part of the Assignment you will create variables of different types and print the output using a formatted string:
+    # 1. Create a program called due_date.py
+    # 2. Prompt the user for the day, month, year, time (minutes), time (hours), and time (seconds) this Assignment is due and store each of those 6 values in a variable. Please use the actual assignment Due Date from Canvas as your inputs.
+day = input("Enter the day (DD): ")
+month = input("Enter the month (MM): ")
+year = input("Enter the year (YYYY): ")
+hours = input("Enter the hours (HH): ")
+minutes = input("Enter the minutes (MM): ")
+seconds = input("Enter the seconds (SS): ")
+# 3. Use the ‘print’ statement to concatenate the variables into a string and output the due
+# date of the assignment in the following format:
+# a. “Module 1 Assignment is due on MM/DD/YYYY at HH:MM:SS PM EST”
+    # i. Fill in MM, DD, YYYY, HH, MM, and SS using the corresponding variables
+    # ii. Your output should not include the quotation marks
+
+print(f"Module 1 Assignment is due on {month}/{day}/{year} at {hours}:{minutes}:{seconds} PM EST")
---
+++ Module 1/Assignments/hello.py
@@ -0,0 +1,12 @@
+# 1. Using the IDE of your choice, create a Python program in a file named hello.py that does the
+# following:
+
+# a. Prompts the user for their name (use your first name as input) and stores it in a variable
+    # i. Prompt should say: “Enter a name: ”
+
+name = input("Enter a name: ")
+
+# b. Prints the string “Hello, <name>!” to the screen.
+   # i. Sample output: Hello, Joe!
+
+print(f"Hello, {name}!")
