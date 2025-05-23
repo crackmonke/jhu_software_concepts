082e2e547c24fa882606d332eca2d2e11b77d50b
crackmonke <savy.greeley@icloud.com>
2025-05-22 18:47:39 -0700

    application factory

--- Module 1/rp_flask_board/board/__init__.py
+++ Module 1/rp_flask_board/board/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Hello, World!"
+def create_app():
+    app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    return app
