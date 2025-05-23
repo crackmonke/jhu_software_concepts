17e717d95377c172ce7748b3da1de5162f9cc5ab
crackmonke <savy.greeley@icloud.com>
2025-05-22 21:06:52 -0700

    readings setup

---
+++ Module 1/personal website/board/pages.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint("pages", __name__)
+
+@bp.route("/")
+def home():
+    return render_template("pages/home.html")
+
+@bp.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
