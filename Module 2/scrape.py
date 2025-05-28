8b9ab0a25f419205f4ec5f51d59a35cbe7d4d12b
crackmonke <savy.greeley@icloud.com>
2025-05-27 19:08:50 -0700

    added to do list

--- Module 2/scrape.py
+++ Module 2/scrape.py
@@ -7,13 +7,26 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find_all("div", class_="tw-gap-2 tw-flex tw-flex-wrap")
+results = soup.find_all("tbody", class_="tw-divide-y tw-divide-gray-200 tw-bg-white")
 
+print(results)
 
 for result in results:  
-    application_status = result.find("div", class_="tw-inline-flex tw-items-center tw-rounded-md tw-bg-sky-50 tw-text-sky-700 tw-ring-sky-600/20 tw-px-2 tw-py-1 tw-text-xs tw-font-medium tw-ring-1 tw-ring-inset md:tw-hidden")
-    if application_status:
-        print(application_status.get_text(strip=True))
-    else:
-        print("No application status found")
-
+    data_rows = result.find("div", class_="tw-divide-y tw-divide-gray-200 tw-bg-white")
+    if data_rows:
+        # program_name
+        # university_name
+        # comments (if available)
+        # date_added
+        #url_to_applicant_entry
+        # application_status
+        ##if accepted, then acceptance_date
+        ##if rejected, then rejection_date
+        #Semester and year of program start (if available)
+        #international_or_american_student (if available)
+        #GRE_score (if available)
+        #GRE_V_score (if available)
+        #Master's_or_PhD (if available)
+        # GPA (if available)
+        # GRE_AW_score (if available)
+        pass
