0adbff887979199cfff69a34bc53f0cb60c975e3
crackmonke <savy.greeley@icloud.com>
2025-05-27 18:54:32 -0700

    Update scrape.py

--- Module 2/scrape.py
+++ Module 2/scrape.py
@@ -2,12 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = "https://www.thegradcafe.com/"
+URL = "https://www.thegradcafe.com/survey/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="tw-divide-y tw-divide-gray-200 tw-bg-white")
+results = soup.find_all("div", class_="tw-gap-2 tw-flex tw-flex-wrap")
 
-print(results)
+
+for result in results:  
+    application_status = result.find("div", class_="tw-inline-flex tw-items-center tw-rounded-md tw-bg-sky-50 tw-text-sky-700 tw-ring-sky-600/20 tw-px-2 tw-py-1 tw-text-xs tw-font-medium tw-ring-1 tw-ring-inset md:tw-hidden")
+    if application_status:
+        print(application_status.get_text(strip=True))
+    else:
+        print("No application status found")
 
