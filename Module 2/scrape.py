5f99ebfcdec0892faa36e278e652b9704047a720
crackmonke <savy.greeley@icloud.com>
2025-05-28 22:57:31 -0700

    returns a mess

--- Module 2/scrape.py
+++ Module 2/scrape.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 # Url to scape - does not include the page number by itself
 url = "https://www.thegradcafe.com/survey/?page="
 
-data_table = soup.find('table', class_='tw-min-w-full tw-divide-y tw-divide-gray-300')
+
 
 # Program Name
 # o University
@@ -25,26 +25,47 @@ data_table = soup.find('table', class_='tw-min-w-full tw-divide-y tw-divide-gray
 
 def scrape_data(max_pages=10):
     page_number = 1
+    collected_data = []
 
     while page_number <= max_pages:
         print(f"Scraping page {page_number}...")
         try:
             current_url = f"{url}{page_number}"
-            page = urlopen(current_url)
-            html = page.read().decode('utf-8')
+            soup = BeautifulSoup(urlopen(current_url), 'html.parser')
+            data_table = soup.find('table', class_='tw-min-w-full tw-divide-y tw-divide-gray-300')
 
         except:
             break
 
         # grab groupings of data
-        groups = soup.find_all('tr', class_=False)
+        groups = data_table.find_all('tr', class_=False)
 
         for group in groups:
+            print(f"Processing group: {group}")
             data = {}
 
+            # School Name
+            school_name = group.find('td', class_='tw-font-medium tw-text-gray-900 tw-text-sm')
+            if school_name:
+                data['school_name'] = school_name.get_text(strip=True)
+            else:
+                data['school_name'] = None
 
-       
+            # Program Name
+            program_name = group.find('td', class_='tw-text-gray-900')
+            if program_name:
+                data['program_name'] = program_name.get_text(strip=True)
+            else:
+                data['program_name'] = None
 
+            if data['program_name'] or data['school_name']:
+                collected_data.append(data)
 
+        # Move to the next page
+        page_number += 1
 
+    return collected_data
 
+entries = scrape_data(3)
+for entry in entries:
+    print(entry)
