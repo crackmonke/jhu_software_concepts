6c91483f92208aa94b5709b3f5c1ff15d9aa69d9
Savy <81570641+crackmonke@users.noreply.github.com>
2025-06-04 17:41:39 -0700

    update to match table requirements

---
+++ module_3/GradCafe/applicants.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[ApplicationData] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  program TEXT NOT NULL, -- Combined school_name and program
+  degree TEXT NOT NULL,
+  date_added DATE NOT NULL,
+  status TEXT NOT NULL, -- was decision
+  url TEXT NOT NULL, -- was result_url
+  term TEXT NOT NULL, -- was semester_year
+  us_or_international TEXT NOT NULL, -- was international_american
+  gpa FLOAT NULL,
+  gre_v_score FLOAT NULL,
+  gre_aw_score FLOAT NULL,
+  gre_score FLOAT NULL,
+  comments TEXT NULL -- was comment
+)
