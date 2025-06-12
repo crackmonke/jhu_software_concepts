12b2c77453ed6b4b54147fdadb0251486f56a1e6
Savy <81570641+crackmonke@users.noreply.github.com>
2025-06-11 21:52:47 -0700

    initalizes a pizza

--- module_4/pizza.py
+++ module_4/pizza.py
@@ -1,35 +1,25 @@
-class pizza
+class pizza:
+    
+    crusts = {"Thin": 5, "Thick": 6, "Gluten Free": 8}
+    sauces = {"Marinara": 2, "Pesto": 3, "Liv Sauce": 5}
+    toppings = {"Pineapple": 1, "Pepperoni": 2, "Mushrooms": 3}
+    cheeses = {"Mozzarella": 0}
 
-#Pizza objects and assoicated cost
-def __init__(self, crust, sauce, cheese, toppings):
-    # Initializes a pizza
-    pizza = None  # Placeholder for pizza object
-    #set pizza variables
-    self.crust = crust
-    self.sauce = sauce
-    self.cheese = cheese
-    self.toppings = toppings
-    self.cost = 0  # Initialize cost to zero
+    #Pizza objects and assoicated cost
+    def __init__(self, crust, sauce, cheese, toppings):
+        # Initalizes a pizza and sets its variables
+        self.crust = crust
+        self.sauce = sauce
+        self.cheese = cheese
+        self.toppings = toppings
 
-    # Calculate cost based on ingredients
-    if crust == "thin":
-        self.cost += 5
-    elif crust == "thick":
-        self.cost += 6
-    elif crust == "Gluten Free":
-        self.cost += 8
+        # Set cost to create
+        self.cost = self.crusts[crust] + self.sauces[sauce] + self.cheeses[cheese] + sum(self.toppings[topping] for topping in toppings)
 
-    if sauce == "Marinara":
-        self.cost += 2
-    elif sauce == "Pesto":
-        self.cost += 3
-    elif sauce == "LivSauce":
-        self.cost += 5
-    #set cost to create
 
-def __str__(self):
-    #Prints a pizza order
-    #Prints the cost of the pizza
+    def __str__(self):
+        #Prints a pizza order
+        #Prints the cost of the pizza
 
-def cost(self):
-    #determines the cost of the pizza
+    def cost(self):
+        #determines the cost of the pizza
