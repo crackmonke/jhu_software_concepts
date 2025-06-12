d0cd63a5747a891e39e31f0f1ba690dd9c35aa65
Savy <81570641+crackmonke@users.noreply.github.com>
2025-06-11 21:55:11 -0700

    pizza finished

--- module_4/pizza.py
+++ module_4/pizza.py
@@ -1,5 +1,5 @@
 class pizza:
-    
+
     crusts = {"Thin": 5, "Thick": 6, "Gluten Free": 8}
     sauces = {"Marinara": 2, "Pesto": 3, "Liv Sauce": 5}
     toppings = {"Pineapple": 1, "Pepperoni": 2, "Mushrooms": 3}
@@ -12,14 +12,20 @@ class pizza:
         self.sauce = sauce
         self.cheese = cheese
         self.toppings = toppings
-
+        
         # Set cost to create
-        self.cost = self.crusts[crust] + self.sauces[sauce] + self.cheeses[cheese] + sum(self.toppings[topping] for topping in toppings)
-
+        self.cost = 0.0
 
     def __str__(self):
-        #Prints a pizza order
-        #Prints the cost of the pizza
+        #Prints a pizza order and its cost
+        return f"Pizza with {self.crust} crust, {self.sauce} sauce, {self.cheese} cheese, and toppings: {', '.join(self.toppings)}. Cost: ${self.cost:.2f}"
+        
 
     def cost(self):
         #determines the cost of the pizza
+        self.cost = (self.crusts[self.crust] + 
+                     self.sauces[self.sauce] + 
+                     self.cheeses[self.cheese] + 
+                     sum(self.toppings[topping] for topping in self.toppings))
+        
+        return self.cost
